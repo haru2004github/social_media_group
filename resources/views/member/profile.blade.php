6fad38fa0e3f63fb22dc55a4676426b6bde23622
Bhone Pyae Sone <hponepyaesone9620@gmail.com>
2024-09-09 23:46:28 +0630

    upload my project

--- resources/views/member/profile.blade.php
+++ resources/views/member/profile.blade.php
@@ -47,11 +47,13 @@
                         @endif
                     </div>
 
+                    @if ($member->id == Auth::user()->id)
                     <div class="absolute bottom-3 right-0 2xl:bottom-5 2xl:right-5">
-                        <button onclick="toggleDisplayForProFile()" class="w-8 h-8 md:w-12 md:h-12 flex justify-center items-center rounded-full shadow-md bg-blue-400 hover:bg-blue-500 text-white duration-300">
+                        <button onclick="toggleDisplayForProFile()" class=" w-8 h-8 md:w-12 md:h-12 flex justify-center items-center rounded-full shadow-md bg-blue-400 hover:bg-blue-500 text-white duration-300">
                             <svg class="w-5 h-5 md:w-auto md:h-auto" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 36 36"><path fill="currentColor" d="M32 4H4a2 2 0 0 0-2 2v24a2 2 0 0 0 2 2h28a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2ZM4 30V6h28v24Z" class="clr-i-outline clr-i-outline-path-1"/><path fill="currentColor" d="M8.92 14a3 3 0 1 0-3-3a3 3 0 0 0 3 3Zm0-4.6A1.6 1.6 0 1 1 7.33 11a1.6 1.6 0 0 1 1.59-1.59Z" class="clr-i-outline clr-i-outline-path-2"/><path fill="currentColor" d="m22.78 15.37l-5.4 5.4l-4-4a1 1 0 0 0-1.41 0L5.92 22.9v2.83l6.79-6.79L16 22.18l-3.75 3.75H15l8.45-8.45L30 24v-2.82l-5.81-5.81a1 1 0 0 0-1.41 0Z" class="clr-i-outline clr-i-outline-path-3"/><path fill="none" d="M0 0h36v36H0z"/></svg>
                         </button>
                     </div>
+                    @endif
                 </div>
 
                 {{-- //mail to  --}}
